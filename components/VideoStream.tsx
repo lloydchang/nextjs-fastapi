ff496c42d77c3535fb17c6c9b3fd9c09abb8e7f1
lloydchang <lloydchang@gmail.com>
2024-09-29 02:20:59 -0700

    fix

--- components/VideoStream.tsx
+++ components/VideoStream.tsx
@@ -1,14 +1,16 @@
 // src/components/VideoStream.tsx
+
 import React, { useEffect } from "react";
 import styles from "./VideoStream.module.css"; // Import CSS module for styling
 import classNames from 'classnames'; // Optional: for easier class management
 
 interface VideoStreamProps {
   isCameraOn: boolean;
+  isPiP: boolean; // New prop
   videoRef: React.RefObject<HTMLVideoElement>;
 }
 
-const VideoStream: React.FC<VideoStreamProps> = ({ isCameraOn, videoRef }) => {
+const VideoStream: React.FC<VideoStreamProps> = ({ isCameraOn, isPiP, videoRef }) => {
   useEffect(() => {
     if (!isCameraOn && videoRef.current) {
       videoRef.current.srcObject = null;
@@ -18,7 +20,7 @@ const VideoStream: React.FC<VideoStreamProps> = ({ isCameraOn, videoRef }) => {
   return (
     <video
       ref={videoRef}
-      className={classNames(styles.video, { [styles.hidden]: !isCameraOn })}
+      className={classNames(styles.video, { [styles.hidden]: !isCameraOn || isPiP })}
       muted
       playsInline
     />
