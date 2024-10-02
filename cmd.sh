52d742d3857350d0148cf62bc0ec2db9ddcc18fa
lloydchang <lloydchang@gmail.com>
2024-10-02 00:01:10 -0700

    fix

--- cmd.sh
+++ cmd.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -x
 
-curl -fsSL https://ollama.com/install.sh | sh
-ollama serve &
+which ollama || curl -fsSL https://ollama.com/install.sh | sh
+ps auxww | grep ollama || ollama serve &
 sleep 5
 ollama pull llama3.2
 ollama run llama3.2
