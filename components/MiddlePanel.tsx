eb71817340671833b8d5bf808b023803158d057e
lloydchang <lloydchang@gmail.com>
2024-10-02 16:03:53 -0700

    fix

--- components/MiddlePanel.tsx
+++ components/MiddlePanel.tsx
@@ -37,7 +37,7 @@ const MiddlePanel: React.FC = () => {
   const [selectedTalk, setSelectedTalk] = useState<Talk | null>(null);
   const [logs, setLogs] = useState<string[]>([]);
   const [errorDetails, setErrorDetails] = useState<string>('');
-  const [greeting, setGreeting] = useState<string>("Hi!");
+  const [greeting, setGreeting] = useState<string>("");
 
   const addLog = (message: string) => {
     setLogs((prevLogs) => [...prevLogs, message]);
