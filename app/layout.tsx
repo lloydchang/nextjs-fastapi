6c14d0d23144ccf6ae615c2ca589e3ffd385825f
lloydchang <lloydchang@gmail.com>
2024-10-02 01:36:10 -0700

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "TEDxSDG",
+  title: "TEDx SDG",
 };
 
 export default function RootLayout({
