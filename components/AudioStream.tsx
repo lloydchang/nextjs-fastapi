4d242f0185541e51cf6b2602804bb40d236b7175
lloydchang <lloydchang@gmail.com>
2024-10-02 10:47:36 -0700

    fix

--- components/AudioStream.tsx
+++ components/AudioStream.tsx
@@ -13,7 +13,7 @@ const AudioStream: React.FC<AudioStreamProps> = React.memo(({ isMicOn, audioRef 
     <audio
       ref={audioRef}
       className={styles.audio}
-      muted={!isMicOn}  // Mute the audio if the microphone is off
+      muted
       autoPlay
       style={{ display: isMicOn ? 'block' : 'none' }}
     />
