beb6f0b5381fa331477cccfcd97dcf7d40469b89
lloydchang <lloydchang@gmail.com>
2024-10-02 01:37:29 -0700

    Revert "fix"
    This reverts commit 6c14d0d23144ccf6ae615c2ca589e3ffd385825f.

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "TEDx SDG",
+  title: "TEDxSDG",
 };
 
 export default function RootLayout({
