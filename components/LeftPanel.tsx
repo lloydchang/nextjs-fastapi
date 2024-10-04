ac5464b8d3c8ad85a7e05cef422fbc065c81c6de
lloydchang <lloydchang@gmail.com>
2024-10-04 16:47:46 -0700

    fix

--- components/LeftPanel.tsx
+++ components/LeftPanel.tsx
@@ -1,6 +1,6 @@
 // components/LeftPanel.tsx
 
-import React, { useState, useRef, useCallback } from 'react';
+import React, { useState } from 'react';
 import Image from 'next/image';
 import BackgroundImage from '../public/TEDxSDG.jpg';
 import { useChat } from '../hooks/useChat';
@@ -13,44 +13,33 @@ import styles from '../styles/LeftPanel.module.css';
 import { useMedia } from '../hooks/useMedia';
 
 const HeavyChatMessages = dynamic(() => import('./ChatMessages'), {
-  loading: () => <div className={styles.emptyPlaceholder}></div>,
+  loading: () => <div className={styles.emptyPlaceholder}></div>, // Invisible placeholder to maintain layout
   ssr: false,
 });
 
 const LeftPanel: React.FC = () => {
   const { mediaState, videoRef, audioRef, startCam, stopCam, toggleMic, togglePip, toggleMem } = useMedia();
-  const { messages, setMessages, sendActionToChatbot, clearChatHistory } = useChat({ isMemOn: mediaState.isMemOn });
+  const { messages, sendActionToChatbot, clearChatHistory } = useChat({ isMemOn: mediaState.isMemOn });
 
   const [chatInput, setChatInput] = useState<string>('');
   const [error, setError] = useState<string | null>(null);
 
-  const chatContainerRef = useRef<HTMLDivElement>(null);
-
-  const handleChat = useCallback(
-    (input: string, isManual = false) => {
-      const messageToSend = input.trim();
-      
-      if (messageToSend) {
-        const prefix = isManual ? "" : "🎙️ ";
-        const formattedMessage = `${prefix}${messageToSend}`;
-        sendActionToChatbot(formattedMessage); // Directly send to the chatbot
-        setChatInput(''); // Clear input after sending
-      }
-    },
-    [sendActionToChatbot]
-  );
-
   return (
     <div className={styles.container}>
       {error && <div className={styles.error}>{error}</div>}
-      <Image src={BackgroundImage} alt="Background" fill className={styles.backgroundImage} />
+      <Image
+        src={BackgroundImage}
+        alt="Background"
+        fill
+        className={styles.backgroundImage}
+      />
       <div className={styles.overlay} />
       <div className={mediaState.isPipOn ? styles.videoStreamHidden : styles.videoStream}>
         <VideoStream isCamOn={mediaState.isCamOn} videoRef={videoRef} />
       </div>
       <AudioStream isMicOn={mediaState.isMicOn} audioRef={audioRef} />
       <div className={styles.content}>
-        <div className={styles.chatInterface} ref={chatContainerRef}>
+        <div className={styles.chatInterface}>
           <HeavyChatMessages messages={messages} />
           <ChatInput 
             chatInput={chatInput} 
