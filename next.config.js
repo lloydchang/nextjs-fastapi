357f928870698140232c64b03815039078f9c1e3
lloydchang <lloydchang@gmail.com>
2024-10-06 08:56:49 -0700

    fix

--- next.config.js
+++ next.config.js
@@ -1,40 +1,38 @@
+// File: next.config.js
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  rewrites: async () => {
+  async rewrites() {
     return [
       {
         source: "/api/py/:path*",
         destination:
           process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8000/api/py/:path*"
+            ? "http://localhost:8000/api/py/:path*"
             : "/api/",
       },
       {
         source: "/docs",
         destination:
           process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8000/api/py/docs"
+            ? "http://localhost:8000/api/py/docs"
             : "/api/py/docs",
       },
       {
         source: "/openapi.json",
         destination:
           process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8000/api/py/openapi.json"
+            ? "http://localhost:8000/api/py/openapi.json"
             : "/api/py/openapi.json",
       },
     ];
   },
-};
-
-// module.exports = nextConfig;
-
-module.exports = {
   typescript: {
-    ignoreBuildErrors: true,  // Disable TypeScript build errors
+    ignoreBuildErrors: true,  // Disable TypeScript build errors during development
   },
   eslint: {
     ignoreDuringBuilds: true,  // Optional: Disable ESLint during build (if needed)
   },
 };
 
+module.exports = nextConfig;
