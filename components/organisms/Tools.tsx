418e89c5cc63589c8afec01e0ed63670570f942a
lloydchang <lloydchang@gmail.com>
2024-10-07 18:57:50 -0700

    fix

--- components/organisms/Tools.tsx
+++ components/organisms/Tools.tsx
@@ -3,57 +3,58 @@
 import React from 'react';
 import '../../styles/components/organisms/Tools.css';
 
-const Tools: React.FC = () => {  // Removed the `messages` prop from the component
+const Tools: React.FC = () => {
   const openInNewTab = (url: string) => {
     window.open(url, '_blank', 'noopener,noreferrer');
   };
 
   return (
     <div className="tools-container">
-      {/* Localizing Button */}
-      <button
-        className="right-edge-button"
-        onClick={() => openInNewTab('https://www.local2030.org/discover-tools')}
-      >
-        Localizing
-      </button>
+      <div className="button-group">
+        {/* Local Button */}
+        <button
+          className="right-edge-button"
+          onClick={() => openInNewTab('https://www.local2030.org/discover-tools')}
+        >
+          Local
+        </button>
 
-      {/* Learning Button */}
-      <button
-        className="right-edge-button"
-        onClick={() => openInNewTab('https://www.unsdglearn.org/learning/')}
-      >
-        Learning
-      </button>
+        {/* Learn Button */}
+        <button
+          className="right-edge-button"
+          onClick={() => openInNewTab('https://www.unsdglearn.org/learning/')}
+        >
+          Learn
+        </button>
 
-      {/* Financing Button */}
-      <button
-        className="right-edge-button"
-        onClick={() => openInNewTab('https://jointsdgfund.org/sdg-financing')}
-      >
-        Financing
-      </button>
+        {/* Fund Button */}
+        <button
+          className="right-edge-button"
+          onClick={() => openInNewTab('https://jointsdgfund.org/sdg-financing')}
+        >
+          Fund
+        </button>
 
-      {/* Reporting Button */}
-      <button
-        className="right-edge-button"
-        onClick={() => openInNewTab('https://open-sdg.org/community')}
-      >
-        Reporting
-      </button>
+        {/* Report Button */}
+        <button
+          className="right-edge-button"
+          onClick={() => openInNewTab('https://open-sdg.org/community')}
+        >
+          Report
+        </button>
 
-      {/* News Button */}
-      <button
-        className="right-edge-button"
-        onClick={() =>
-          openInNewTab(
-            'https://news.google.com/topics/CAAqJAgKIh5DQkFTRUFvS0wyMHZNSEk0YTI1c1poSUNaVzRvQUFQAQ'
-          )
-        }
-      >
-        News
-      </button>
-
+        {/* News Button */}
+        <button
+          className="right-edge-button"
+          onClick={() =>
+            openInNewTab(
+              'https://news.google.com/topics/CAAqJAgKIh5DQkFTRUFvS0wyMHZNSEk0YTI1c1poSUNaVzRvQUFQAQ'
+            )
+          }
+        >
+          News
+        </button>
+      </div>
     </div>
   );
 };
