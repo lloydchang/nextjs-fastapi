78a0e5d3b24987769f76a6052c98ef5a4bb56b57
lloydchang <lloydchang@gmail.com>
2024-10-07 20:53:13 -0700

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,13 @@
 // File: app/layout.tsx
-import "./globals.css";
-import { Inter } from "next/font/google";
+import './globals.css';
+import { Inter } from 'next/font/google';
+import Script from 'next/script';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: "TEDxSDG",
-  description: "TEDxSDG",
+  title: 'TEDxSDG',
+  description: 'TEDxSDG',
 };
 
 export default function RootLayout({
@@ -20,7 +21,11 @@ export default function RootLayout({
         {/* Add viewport meta tag */}
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        {/* Include the dynamic script */}
+        <Script src="/setContainerSize.js" strategy="afterInteractive" />
+      </body>
     </html>
   );
 }
