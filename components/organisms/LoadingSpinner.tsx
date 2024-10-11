fb6135fe7df27cefd272c1bed49844b30176317b
lloydchang <lloydchang@gmail.com>
2024-10-11 15:44:47 -0700

    fix

--- components/organisms/LoadingSpinner.tsx
+++ components/organisms/LoadingSpinner.tsx
@@ -7,7 +7,15 @@ import styles from 'styles/components/organisms/LoadingSpinner.module.css';
 
 const LoadingSpinner: React.FC = () => (
   <div className={styles.loadingSpinnerContainer}>
-    <Image src={SDGWheel} alt="Loading" width={24} height={24} className={styles.loadingSpinner} />
+    {/* Added unoptimized property to the Image component */}
+    <Image 
+      src={SDGWheel} 
+      alt="Loading" 
+      width={24} 
+      height={24} 
+      className={styles.loadingSpinner} 
+      unoptimized // Prevents optimization for this image
+    />
   </div>
 );
 
