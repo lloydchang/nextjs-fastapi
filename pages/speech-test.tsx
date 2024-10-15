d27a08d77fa0b26d1604ed18ab08cc8b51ae423d
lloydchang <lloydchang@gmail.com>
2024-10-14 21:24:57 -0700

    fix

---
+++ pages/speech-test.tsx
@@ -0,0 +1,14 @@
+// File: pages/speech-test.tsx
+
+import React from 'react';
+import SpeechTest from 'components/atoms/SpeechTest';
+
+const SpeechTestPage: React.FC = () => {
+  return (
+    <div>
+      <SpeechTest />
+    </div>
+  );
+};
+
+export default SpeechTestPage;
