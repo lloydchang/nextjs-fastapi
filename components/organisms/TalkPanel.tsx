62851dbbb520d3b32fa01e64d706aecafbe8d648
lloydchang <lloydchang@gmail.com>
2024-10-16 15:04:11 -0700

    fix

--- components/organisms/TalkPanel.tsx
+++ components/organisms/TalkPanel.tsx
@@ -147,7 +147,7 @@ const TalkPanel: React.FC = () => {
         const sendSdgTag = talk.sdg_tags.length > 0 ? sdgTitleMap[talk.sdg_tags[0]] : '';
 
         await dispatch(
-          sendMessage({ text: `${sendTranscript} | ${sendSdgTag} | ${talk.title} | ${query}`, hidden: true })
+          sendMessage({ text: `${sendTranscript}\n\n${sendSdgTag}\n\n${talk.title}\n\n${query}\n\n`, hidden: true })
         );
         console.log(`Sent message for talk: ${talk.title}`);
       } catch (error) {
