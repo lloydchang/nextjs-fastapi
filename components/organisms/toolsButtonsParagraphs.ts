e6ffed099eed3b2566a50bc64b8559cfd0d8cd0e
lloydchang <lloydchang@gmail.com>
2024-10-17 13:44:02 -0700

    fix

--- components/organisms/toolsButtonsParagraphs.ts
+++ components/organisms/toolsButtonsParagraphs.ts
@@ -1,111 +1,111 @@
 // File: components/organisms/toolsButtonsParagraphs.ts
-// Mapping buttons to paragraphs and URLs
+// Mapping buttons to concise calls to action and URLs
 
 const buttonparagraphs: { [key: string]: { paragraph: string; url: string } } = {
-    Lazy: {
-      paragraph: `Discover easy ways to contribute to the UN's Sustainable Development Goals.`,
-      url: 'https://www.un.org/sustainabledevelopment/takeaction/',
-    },
-    Water: {
-      paragraph: `Learn simple actions to protect water and contribute to the UN's Sustainable Development Goals.`,
-      url: 'https://www.un.org/sustainabledevelopment/the-lazy-persons-guide-to-saving-water/',
-    },
-    Hero: {
-      paragraph: `Meet the Climate Action Superheroes and learn how to join the fight against climate change.`,
-      url: 'https://www.un.org/sustainabledevelopment/climate-action-superheroes/',
-    },
-    Act: {
-      paragraph: `ActNow is the UN's call to action on Sustainable Development Goals. Join the movement!`,
-      url: 'https://www.un.org/en/actnow',
-    },
-    Daily: {
-      paragraph: `Discover 170 daily actions to transform our world and contribute to the SDGs.`,
-      url: 'https://drive.google.com/file/d/1iMdE6DLLuCqwq3K9U-DaTUWB6KyMa8QG/view',
-    },
-    App: {
-      paragraph: `Download AWorld, the UN's app for sustainable living, track your impact, and join the movement.`,
-      url: 'https://actnow.aworld.org/',
-    },
-    Game: {
-      paragraph: `Learn about the UN Sustainable Development Goals through the Go Goals! board game.`,
-      url: 'https://go-goals.org/',
-    },
-    Learn: {
-      paragraph: `Explore UN SDG:Learn, a platform for learning about sustainable development.`,
-      url: 'https://www.unsdglearn.org/learning/',
-    },
-    Voluntary: {
-      paragraph: `Learn about Voluntary Local Reviews and how local governments are implementing the SDGs.`,
-      url: 'https://sdgs.un.org/topics/voluntary-local-reviews',
-    },
-    Local: {
-      paragraph: `Explore Local2030, a network supporting the on-the-ground delivery of the Sustainable Development Goals.`,
-      url: 'https://www.local2030.org/vlrs',
-    },
-    Review: {
-      paragraph: `Discover how cities are using Voluntary Local Reviews to track SDG progress.`,
-      url: 'https://unhabitat.org/topics/voluntary-local-reviews?order=field_year_of_publication_vlr&sort=desc#block-vlrworldmap',
-    },
-    Lab: {
-      paragraph: `Learn about cities leading on Voluntary Local Reviews through the online VLR Lab.`,
-      url: 'https://www.iges.or.jp/en/projects/vlr',
-    },
-    City: {
-      paragraph: `See which cities are using the Open SDG platform to manage and publish data related to the SDGs.`,
-      url: 'https://open-sdg.org/community#cities-and-regions',
-    },
-    Country: {
-      paragraph: `Explore SDG Country Profiles and access data on progress towards the Sustainable Development Goals.`,
-      url: 'https://unstats.un.org/sdgs/dataportal/countryprofiles',
-    },
-    Global: {
-      paragraph: `Learn about the Global Sustainable Development Report and its role in the science-policy interface.`,
-      url: 'https://sdgs.un.org/gsdr',
-    },
-    Annual: {
-      paragraph: `Explore the 17 Sustainable Development Goals and learn about their history and implementation.`,
-      url: 'https://sdgs.un.org/goals',
-    },
-    Rank: {
-      paragraph: `Access and explore human development data and rankings for countries worldwide.`,
-      url: 'https://hdr.undp.org/data-center/country-insights#/ranks',
-    },
-    Atlas: {
-      paragraph: `Explore the Atlas of Sustainable Development Goals, featuring interactive data visualizations on SDG progress.`,
-      url: 'https://datatopics.worldbank.org/sdgatlas/',
-    },
-    Grant: {
-      paragraph: `Learn about grants for advancing the UN Sustainable Development Goals and promoting collaborations.`,
-      url: 'https://globalaffairs.ucdavis.edu/sdgs-grants',
-    },
-    Fund: {
-      paragraph: `Discover the Joint SDG Fund and its role in accelerating progress towards the Sustainable Development Goals.`,
-      url: 'https://jointsdgfund.org/sdg-financing#PROFILES',
-    },
-    Hub: {
-      paragraph: `Learn about the UN Multi-Partner Trust Fund Office, the UN's center of expertise on pooled funding.`,
-      url: 'https://mptf.undp.org/#impact-to-label',
-    },
-    Aid: {
-      paragraph: `Access data on development and humanitarian activities through the International Aid Transparency Initiative.`,
-      url: 'https://countrydata.iatistandard.org/',
-    },
-    Data: {
-      paragraph: `Explore UN Data, a public repository of authoritative data from across the UN system.`,
-      url: 'https://unstats.un.org/UNSDWebsite/undatacommons/search',
-    },
-    Catalog: {
-      paragraph: `Discover the SDG Data Catalog, a global database of data sets relevant to the Sustainable Development Goals.`,
-      url: 'https://ai4good.org/what-we-do/sdg-data-catalog/',
-    },
-    Tool: {
-      paragraph: `Explore the Local2030 Toolbox and discover tools for implementing the SDGs at the local level.`,
-      url: 'https://www.local2030.org/discover-tools',
-    },
-    News: {
-      paragraph: `Stay up-to-date on the latest news about the Sustainable Development Goals.`,
-      url: 'https://news.google.com/topics/CAAqJAgKIh5DQkFTRUFvS0wyMHZNSEk0YTI1c1poSUNaVzRvQUFQAQ',
-    }
- };
-  
-  export default buttonparagraphs;
+  Lazy: {
+    paragraph: `Take simple steps toward sustainable development.`,
+    url: 'https://www.un.org/sustainabledevelopment/takeaction/',
+  },
+  Water: {
+    paragraph: `Help protect water with easy actions.`,
+    url: 'https://www.un.org/sustainabledevelopment/the-lazy-persons-guide-to-saving-water/',
+  },
+  Hero: {
+    paragraph: `Join the fight with Climate Action Superheroes.`,
+    url: 'https://www.un.org/sustainabledevelopment/climate-action-superheroes/',
+  },
+  Act: {
+    paragraph: `Join ActNow and support global sustainability.`,
+    url: 'https://www.un.org/en/actnow',
+  },
+  Daily: {
+    paragraph: `Transform the world with 170 daily actions.`,
+    url: 'https://drive.google.com/file/d/1iMdE6DLLuCqwq3K9U-DaTUWB6KyMa8QG/view',
+  },
+  App: {
+    paragraph: `Track your impact with the AWorld app.`,
+    url: 'https://actnow.aworld.org/',
+  },
+  Game: {
+    paragraph: `Learn SDGs through the Go Goals! game.`,
+    url: 'https://go-goals.org/',
+  },
+  Learn: {
+    paragraph: `Discover SDG learning resources and courses.`,
+    url: 'https://www.unsdglearn.org/learning/',
+  },
+  Voluntary: {
+    paragraph: `Explore local SDG implementation efforts.`,
+    url: 'https://sdgs.un.org/topics/voluntary-local-reviews',
+  },
+  Local: {
+    paragraph: `Support SDGs with Local2030 initiatives.`,
+    url: 'https://www.local2030.org/vlrs',
+  },
+  Review: {
+    paragraph: `Track progress with Voluntary Local Reviews.`,
+    url: 'https://unhabitat.org/topics/voluntary-local-reviews?order=field_year_of_publication_vlr&sort=desc#block-vlrworldmap',
+  },
+  Lab: {
+    paragraph: `Explore VLR Lab for locally-led transformations for SDGs.`,
+    url: 'https://www.iges.or.jp/en/projects/vlr',
+  },
+  City: {
+    paragraph: `Discover cities using Open SDG platforms.`,
+    url: 'https://open-sdg.org/community#cities-and-regions',
+  },
+  Country: {
+    paragraph: `Access SDG country profiles and data.`,
+    url: 'https://unstats.un.org/sdgs/dataportal/countryprofiles',
+  },
+  Global: {
+    paragraph: `Explore the Global SDG Report insights.`,
+    url: 'https://sdgs.un.org/gsdr',
+  },
+  Annual: {
+    paragraph: `Learn about the 17 SDGs and their history.`,
+    url: 'https://sdgs.un.org/goals',
+  },
+  Rank: {
+    paragraph: `Explore human development rankings worldwide.`,
+    url: 'https://hdr.undp.org/data-center/country-insights#/ranks',
+  },
+  Atlas: {
+    paragraph: `Visualize SDG progress with the Atlas.`,
+    url: 'https://datatopics.worldbank.org/sdgatlas/',
+  },
+  Grant: {
+    paragraph: `Find grants for SDG initiatives.`,
+    url: 'https://globalaffairs.ucdavis.edu/sdgs-grants',
+  },
+  Fund: {
+    paragraph: `Support SDG progress with joint funds.`,
+    url: 'https://jointsdgfund.org/sdg-financing#PROFILES',
+  },
+  Hub: {
+    paragraph: `Discover pooled funding initiatives.`,
+    url: 'https://mptf.undp.org/#impact-to-label',
+  },
+  Aid: {
+    paragraph: `Access global aid data insights.`,
+    url: 'https://countrydata.iatistandard.org/',
+  },
+  Data: {
+    paragraph: `Explore UN's data repository.`,
+    url: 'https://unstats.un.org/UNSDWebsite/undatacommons/search',
+  },
+  Catalog: {
+    paragraph: `Access SDG data catalog globally.`,
+    url: 'https://ai4good.org/what-we-do/sdg-data-catalog/',
+  },
+  Tool: {
+    paragraph: `Use tools to implement SDGs locally.`,
+    url: 'https://www.local2030.org/discover-tools',
+  },
+  News: {
+    paragraph: `Stay updated on SDG news.`,
+    url: 'https://news.google.com/topics/CAAqJAgKIh5DQkFTRUFvS0wyMHZNSEk0YTI1c1poSUNaVzRvQUFQAQ',
+  },
+};
+
+export default buttonparagraphs;
