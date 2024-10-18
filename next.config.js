8db090821b11c8e8ffe6899e26dd838ea6546f2e
lloydchang <lloydchang@gmail.com>
2024-10-17 22:26:03 -0700

    fix

--- next.config.js
+++ next.config.js
@@ -49,7 +49,7 @@ const nextConfig = {
 
 module.exports = {
   webpack: (config) => {
-    config.resolve.symlinks = false; // Ensure correct module resolution
+    config.resolve.symlinks = false;
     return config;
   },
 };
